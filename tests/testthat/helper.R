d4f5cddefb9de3022edfe75ef819e2a2690f8033
pmarres <pmarres@oxylo.com>
2023-03-08 13:15:23 +0100

    Added helper.R

---
+++ tests/testthat/helper.R
@@ -0,0 +1,6 @@
+# This is a so called helper script (script prefixed by the word helper)
+# These scripts are run once *before* the tests are run.
+# https://stackoverflow.com/questions/24823353/testthat-in-r-sourcing-in-tested-files
+
+library(readxl)
+
