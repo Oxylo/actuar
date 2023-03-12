61b5917f4d558cd9178fa3973330fc5634129cb7
Pieter Marres <pmarres@quantsense.io>
2023-03-12 21:16:50 +0100

    External packages (like readxl) must be imported in DESCRIPTION !

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -2,8 +2,6 @@
 # These scripts are run once *before* the tests are run.
 # https://stackoverflow.com/questions/24823353/testthat-in-r-sourcing-in-tested-files
 
-library(readxl)
+# library(readxl)         --> external libraries must be imported in DESCRIPTION !!
 
-# source("/home/pieter/packages/actuar/R/pv.R")
-
-# source("../../R/pv.R") Bad habit !;)
+# source("../../R/pv.R")  --> Bad habit !;)
